a3d4b36eb83b5920ff1406a422a1e6c03fb6faf9
TDPrograms <fieldsontori@gmail.com>
2022-10-04 10:11:09 -0700

    fixed more values in combat.py

--- rpg/combat.py
+++ rpg/combat.py
@@ -4,15 +4,15 @@ from operator import truediv
 class Combat():
     def __init__(self):
         #no idea if im doing this right but im trying!!!
-        self.player_characters = "character.py"
-        self.npc = "monster.py"
-        self.interactive_mode = True
+        self.player_characters = []
+        self.npc = []
+        self.interactive_mode = False
         self.party_xp = 0
-        self.party_success = True
-        self.ordered_combatants = 0
+        self.party_success = False
+        self.ordered_combatants = []
         self.player_ply_function = ""
         self.endgame_function = ""
-
+	#are_all_characters_dead
     def characters_dead(self):
         pass
 
